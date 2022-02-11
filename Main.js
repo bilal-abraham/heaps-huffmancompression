629b9c6f91f0228996bc6de19873e4b76cb2754c
bilal-abraham <bilal.abraham.717@gmail.com>
2022-02-11 09:03:01 -0800

    taking flags in args

--- Main.js
+++ Main.js
@@ -1,40 +1,13 @@
 const arg = process.argv.slice(2);
-arg[1] = Number(arg[1]);
-arg[2] = Number(arg[2]);
 switch (arg[0]) {
-	case '+':
-		console.log(`Result of ${arg[1]} 
-        + ${arg[2]} = ${arg[1] + arg[2]}`);
+	case '-o':
+		console.log('-o tried');
 		break;
 
-	case '*':
-		console.log(`Result of ${arg[1]} 
-        * ${arg[2]} = ${arg[1] * arg[2]}`);
-		break;
-
-	case '-':
-		console.log(`Result of ${arg[1]} 
-        - ${arg[2]} = ${arg[1] - arg[2]}`);
-		break;
-
-	case '/':
-		if (arg[2] == 0) {
-			console.log('cannot be divided by zero!!');
-		} else {
-			console.log(`Result of ${arg[1]} 
-            / ${arg[2]} = ${arg[1] / arg[2]}`);
-		}
-		break;
-
-	case '%':
-		if (arg[2] == 0) {
-			console.log('cannot be divided by zero!!');
-		} else {
-			console.log(`Result of ${arg[1]} 
-            % ${arg[2]} = ${arg[1] % arg[2]}`);
-		}
+	case '-b':
+		console.log('-b tried');
 		break;
 
 	default:
-		console.log(`operation cannot be performed!!`);
+		console.log('Bad things inputted');
 }
